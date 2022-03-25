608436d38b3405254358cb3e8a87bbb4f71fe980
Mark Harmon <neothej1sec@gmail.com>
2022-03-25 09:02:50 -0500

    added new controllers and respective routes

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ router.get('/', (req, res) => res.send('This is our project!'))
 
 // app.router('/parks', controllers.addComment)
 //
+router.post('/parks', controllers.addComment)
+router.get('/parks', controllers.getAllParks)
+router.get('/parks/:id', controllers.getParkById)
 module.exports = router;
 
 //
